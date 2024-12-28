b6bd4a31792bd040679843d75583c2756698349f
Deep-Gohil <deep72617626@gmail.com>
2024-12-28 12:37:03 +0530

    Updated

---
+++ myapp/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Test from './test'
+
+export const App = ()=>{
+  return(
+    <div>
+      <h1>React</h1>
+      <Test name="node"/>
+      <Test name="html"/>
+      <Test name="css"/>
+      <Test name="javascript"/>
+    </div>
+  )
+}
---
+++ stateapp/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "./Counter";
+
+const App = ()=>{
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <Counter/>
+            <Counter/>
+            <Counter/>
+            <Counter/>
+        </div>
+    )
+}
+
+export default App;
