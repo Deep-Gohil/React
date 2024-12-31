fb99852dabad770ae84083c412fa8a87b3fac2c5
Deep-Gohil <deep72617626@gmail.com>
2024-12-31 11:53:15 +0530

    State App

--- stateapp/src/App.jsx
+++ stateapp/src/App.jsx
@@ -3,9 +3,6 @@ import Counter from "./Counter";
 const App = ()=>{
     return (
         <div>
-            <h1>Hello World</h1>
-            <Counter/>
-            <Counter/>
             <Counter/>
             <Counter/>
         </div>
--- stateapp/src/Counter.jsx
+++ stateapp/src/Counter.jsx
@@ -3,20 +3,42 @@ import { useState } from "react"
 const Counter = () => {
 
     // let count = 0
-    let [count,setCount] = useState(0)
+    let [count, setCount] = useState(0)
 
     const decrement = () => {
-        setCount(count--)
+        setCount(count - 1)
     }
 
     const increment = () => {
-        setCount(count++)
+        setCount(count + 1)
     }
     return (
-        <div>
-            <button onClick={decrement}>-</button>
-            <button>{count}</button>
-            <button onClick={increment}>+</button>
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "20px",
+        }}>
+            <button style={{
+                backgroundColor: "red",
+                color: "white",
+                height: "30px",
+                width: "30px",
+            }} onClick={decrement}>-</button>
+            <button style={{
+                backgroundColor: "blue",
+                color: "white",
+                height: "30px",
+                width: "30px",
+                marginLeft: "10px",
+            }}>{count}</button>
+            <button style={{
+                backgroundColor: "green",
+                color: "white",
+                height: "30px",
+                width: "30px",
+                marginLeft: "10px",
+            }} onClick={increment}>+</button>
         </div>
     )
 }
